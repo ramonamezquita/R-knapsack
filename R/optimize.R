dcc8735f0e274ca6dbc13cce8fdd6688efaef981
Carmen <carmro18ucm.es>
2024-10-16 16:04:32 +0200

    optimize coments

--- R/optimize.R
+++ R/optimize.R
@@ -12,4 +12,10 @@ profvis({
   brute_force_knapsack(x = knapsack_objects[1:100, ], W = 3500)
 })
 
-#brute_force_knapsack is the most complex function 
+#brute_force_knapsack is the most complex function but we can not optimize more,
+# it has a complexity O(2^n) if there are not much numbers is good to work in parallel
+
+#greedy_knapsack has complexity O(n*log(n)) the fastest one 
+
+#knapsack_dynamic is O(W*n) where n is the number of items and 
+#W is the maximum capacity
